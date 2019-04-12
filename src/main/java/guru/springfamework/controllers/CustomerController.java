ff1aaa429c2e74b30ebfc56c3bcdfa470a9f041e
Eric <eric_gomez_1@hotmail.com>
2019-04-11 17:59:48 -0700

    refactored the customer controller to utilize the RestController annotation

--- src/main/java/guru/springfamework/controllers/CustomerController.java
+++ src/main/java/guru/springfamework/controllers/CustomerController.java
@@ -4,11 +4,7 @@ import guru.springfamework.api.v1.model.CustomerDTO;
 import guru.springfamework.api.v1.model.CustomerListDTO;
 import guru.springfamework.services.CustomerService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customers/")
@@ -22,14 +18,16 @@ public class CustomerController
     }
 
     @GetMapping
-    public ResponseEntity<CustomerListDTO> getAllCustomers()
+    @ResponseStatus(HttpStatus.OK)
+    public CustomerListDTO getAllCustomers()
     {
-        return new ResponseEntity<>(new CustomerListDTO(customerService.getAllCustomers()), HttpStatus.OK);
+        return new CustomerListDTO(customerService.getAllCustomers());
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<CustomerDTO> getCustomer(@PathVariable Long id)
+    @ResponseStatus(HttpStatus.OK)
+    public CustomerDTO getCustomer(@PathVariable Long id)
     {
-        return new ResponseEntity<>(customerService.findById(id), HttpStatus.OK);
+        return customerService.findById(id);
     }
 }
--- src/main/java/guru/springfamework/repositories/CustomerRepository.java
+++ src/main/java/guru/springfamework/repositories/CustomerRepository.java
@@ -1,6 +1,6 @@
 package guru.springfamework.repositories;
 
-import guru.springfamework.api.v1.model.CustomerDTO;
+
 import guru.springfamework.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
